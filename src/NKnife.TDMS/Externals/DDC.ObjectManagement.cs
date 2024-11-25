aa27467b58f9dec807611d63eb01b6894f4e146b
lukan <lukan@live.cn>
2024-11-25 21:20:41 +0800

    重构 DDC 类及其 OpenFile 方法
    删除了 DDC.ObjectManagement-Obsolete.cs 文件中的不必要 using 语句，并删除了 NKnife.TDMS.Externals 命名空间下的 DDC 类及其 OpenFile 方法。
    在 DDC.ObjectManagement.cs 文件中，添加了 using NKnife.TDMS.Common 语句。
    修改了 DDC 类的注释部分，将 <br/> 标签替换为 <br />。
    添加了新的 OpenFile 方法的注释，详细描述了该方法的参数和功能。
    重命名 OpenFileEx 方法为 OpenFile，并调整了参数顺序。
    添加了新的 OpenFile 方法的声明，使用 DllImport 属性从 DLL 中导入该方法。

--- src/NKnife.TDMS/Externals/DDC.ObjectManagement.cs
+++ src/NKnife.TDMS/Externals/DDC.ObjectManagement.cs
@@ -7,18 +7,18 @@ namespace NKnife.TDMS.Externals
     internal partial class DDC : DIAdemConnectivity
     {
         /// <summary>
-        /// 创建一个新的TDMS文件。<br/>
-        /// 推荐使用最新版本的TDM流文件格式(DDC_FILE_TYPE_TDM_STREAMING)创建.tdms文件。<br/>
-        /// 注意：如果您在filePath参数中指定的文件扩展名是.tdm或.tdms，那么您可以传递NULL或空字符串来自动使用文件扩展名来指定文件类型。<br/>
-        /// 但是如果指定了文件类型，那么必须和filePath参数指定的文件扩展名是匹配的。<br/>
-        /// 通过传递DDC_FILE_TYPE_TDM来创建一个.tdm文件。通过传递DDC_FILE_TYPE_TDMS来创建.tdms文件。
+        ///     创建一个新的TDMS文件。<br />
+        ///     推荐使用最新版本的TDM流文件格式(DDC_FILE_TYPE_TDM_STREAMING)创建.tdms文件。<br />
+        ///     注意：如果您在filePath参数中指定的文件扩展名是.tdm或.tdms，那么您可以传递NULL或空字符串来自动使用文件扩展名来指定文件类型。<br />
+        ///     但是如果指定了文件类型，那么必须和filePath参数指定的文件扩展名是匹配的。<br />
+        ///     通过传递DDC_FILE_TYPE_TDM来创建一个.tdm文件。通过传递DDC_FILE_TYPE_TDMS来创建.tdms文件。
         /// </summary>
         /// <param name="filePath">文件路径</param>
         /// <param name="fileType">
-        /// 要创建的文件类型。<br/>
-        /// 传递DDC_FILE_TYPE_TDM_STREAMING将使用安装支持的最新版本的TDM流文件格式创建一个.tdms文件。<br/>
-        /// 通过传递DDC_FILE_TYPE_TDM来创建一个.tdm文件。通过传递DDC_FILE_TYPE_TDMS来创建.tdms文件。<br/>
-        /// 如果您在filePath参数中指定的文件扩展名是.tdm或.tdms，那么您可以传递NULL或空字符串来自动使用文件扩展名来指定文件类型。
+        ///     要创建的文件类型。<br />
+        ///     传递DDC_FILE_TYPE_TDM_STREAMING将使用安装支持的最新版本的TDM流文件格式创建一个.tdms文件。<br />
+        ///     通过传递DDC_FILE_TYPE_TDM来创建一个.tdm文件。通过传递DDC_FILE_TYPE_TDMS来创建.tdms文件。<br />
+        ///     如果您在filePath参数中指定的文件扩展名是.tdm或.tdms，那么您可以传递NULL或空字符串来自动使用文件扩展名来指定文件类型。
         /// </param>
         /// <param name="name">TDMS文件中的name属性</param>
         /// <param name="description">TDMS文件中的description属性</param>
@@ -55,10 +55,31 @@ namespace NKnife.TDMS.Externals
         [DllImport(DLL, EntryPoint = "DDC_CloseFile", CallingConvention = CallingConvention.StdCall)]
         public static extern int CloseFile(IntPtr file);
 
+        /// <summary>
+        /// 打开现有TDMS数据文件。
+        /// </summary>
+        /// <param name="filePath">打开文件的路径。如果不传递绝对路径名，则路径名是相对于当前工作目录。</param>
+        /// <param name="fileType">要打开的文件类型。通过TDM打开. TDM文件。通过TDMS打开. TDMS文件。如果在filePath参数中指定的文件扩展名是.tdm或.tdms，那么可以传递NULL或空字符串来自动使用文件扩展名来指定文件类型。</param>
+        /// <param name="readOnly">
+        /// 文件句柄的只读标记<br/>
+        /// - 指定一个非零值以只允许读访问;<br/>
+        /// - 指定0以允许对文件进行读写访问。
+        /// </param>
+        /// <param name="file">输出TDMS数据文件</param>
         [DllImport(DLL, EntryPoint = "DDC_OpenFileEx", CallingConvention = CallingConvention.StdCall)]
-        public static extern int OpenFileEx(string filePath,
-                                            string fileType,
-                                            int readOnly,
-                                            out IntPtr file);
+        public static extern int OpenFile(string filePath,
+                                          string fileType,
+                                          int readOnly,
+                                          out IntPtr file);
+
+        /// <summary>
+        /// 打开现有TDMS数据文件。
+        /// </summary>
+        /// <param name="filePath">打开文件的路径。如果不传递绝对路径名，则路径名是相对于当前工作目录。</param>
+        /// <param name="fileType">要打开的文件类型。通过TDM打开. TDM文件。通过TDMS打开. TDMS文件。如果在filePath参数中指定的文件扩展名是.tdm或.tdms，那么可以传递NULL或空字符串来自动使用文件扩展名来指定文件类型。</param>
+        /// <param name="file">输出TDMS数据文件</param>
+        [DllImport(DLL, CallingConvention = CallingConvention.StdCall, EntryPoint = "DDC_OpenFile")]
+        public static extern int OpenFile(string filePath, string fileType, out IntPtr file);
+
     }
 }
