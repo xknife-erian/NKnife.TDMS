1a7d5e4710f1dec3609fac126dd3573915609b3b
lukan <lukan@live.cn>
2024-11-26 18:05:53 +0800

    添加 NKnife.TDMSDataViewer 项目及其相关配置
    在解决方案中添加了 NKnife.TDMSDataViewer 项目，并配置了不同平台的调试和发布设置。新增了 WPF 应用程序定义和启动 URI。定义了 App 类和主题资源字典。配置了项目文件，目标框架为 net8.0-windows，并添加了必要的 NuGet 包引用。添加了多个类用于初始化停靠布局、选择样式和数据模板。定义了 Workbench 窗口及其视图模型，并设置了 DockingManager 的主题。

---
+++ sample/NKnife.TDMSDataViewer/Views/Workbench.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using AvalonDock.Layout;
+using AvalonDock.Themes;
+
+namespace NKnife.TDMSDataViewer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DockingManager.Theme = new Vs2013BlueTheme();
+        }
+
+        private void OnPreviewMouseRightDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.OriginalSource is not FrameworkElement frameworkElement)
+                return;
+
+            switch (frameworkElement.DataContext)
+            {
+                case LayoutDocument layoutDocument:
+                    layoutDocument.IsSelected = true;
+                    layoutDocument.IsActive   = true;
+
+                    break;
+                case LayoutAnchorable layoutAnchorable:
+                    layoutAnchorable.IsSelected = true;
+                    layoutAnchorable.IsActive   = true;
+
+                    break;
+            }
+        }
+    }
+}
