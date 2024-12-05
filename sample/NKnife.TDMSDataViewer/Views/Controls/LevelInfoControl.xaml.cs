fb44ee9c35b6294bee3c405e4ab6416410f6a33e
lukan <lukan@live.cn>
2024-12-05 23:53:42 +0800

    重构 LevelInfoControl 结构并更新描述文本
    在 `LevelInfoControl.xaml` 中，删除了原有的 `StackPanel` 及其资源定义，并将其替换为一个包含 `ScrollViewer` 的新结构。新的结构中添加了一个 `ListBox`，并使用了新的 `Collection2ListVisibilityCvt` 转换器来控制其可见性。
    
    在 `LevelInfoControl.xaml.cs` 中，添加了 `System.Collections.ObjectModel` 的引用，并新增了 `Collection2ListVisibilityCvt` 类，用于根据集合的数量来控制可见性。
    
    在 `SampleDataCreateWizard.xaml` 中，更新了向导页面的描述文本，将英文描述替换为中文描述。

--- sample/NKnife.TDMSDataViewer/Views/Controls/LevelInfoControl.xaml.cs
+++ sample/NKnife.TDMSDataViewer/Views/Controls/LevelInfoControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -37,6 +38,29 @@ namespace NKnife.TDMSDataViewer.Views.Controls
                                CultureInfo culture)
         {
             return value is bool and true ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <inheritdoc />
+        public object? ConvertBack(object? value,
+                                   Type targetType,
+                                   object? parameter,
+                                   CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+        #endregion
+    }
+
+    class Collection2ListVisibilityCvt : IValueConverter
+    {
+        #region Implementation of IValueConverter
+        /// <inheritdoc />
+        public object? Convert(object? value,
+                               Type targetType,
+                               object? parameter,
+                               CultureInfo culture)
+        {
+            return value is int count ? count <= 0 ? Visibility.Hidden : Visibility.Visible : Visibility.Hidden;
         }
 
         /// <inheritdoc />
